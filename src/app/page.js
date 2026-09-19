aad97307cbb90dfd0c7f66d224981444e280f62e
Tuguldur T <146443087+tuuguu556@users.noreply.github.com>
2026-09-19 22:25:22 +0800

    main idea

--- src/app/page.js
+++ src/app/page.js
@@ -1,69 +1,152 @@
-import Image from "next/image";
+"use client";
+
+import { useRef, useState } from "react";
 
 export default function Home() {
+  const [imageSrc, setImageSrc] = useState(null);
+  const [algorithmSelected, setAlgorithmSelected] = useState("Floyd-Steinberg");
+  const algorithms = ["Bayer 4x4", "Floyd-Steinberg"];
+
+  const canvasRef = useRef(null);
+  const loadedImgRef = useRef(null);
+
+  const pallete = [
+    [15, 56, 15],
+    [48, 98, 48],
+    [139, 172, 15],
+    [155, 188, 15],
+  ]
+
+  const findClosestColor = (r, g, b, pallete) => {
+    let minDistance = Infinity;
+    let closestColor = pallete[0];
+
+    for(let i = 0; i < pallete.length; i++) {
+      const [pr, pg, pb] = pallete[i];
+      const distance = (r-pr) ** 2 + (g-pg) ** 2 + (b-pb) ** 2;
+
+      if(distance < minDistance) {
+        minDistance = distance;
+        closestColor = pallete[i];
+      }
+    }
+    return closestColor;
+  };
+  const floydsteinberg = (data, width, height, pallete) => {
+    const calculateError = (nx, ny, errR, errG, errB, factor) => {
+      if(nx >= 0 && nx < width && ny >= 0 && ny < height) {
+        let nIdx = (ny * width + nx) * 4;
+        data[nIdx] += errR *factor
+        data[nIdx + 1] += errG * factor;
+        data[nIdx + 2] += errB * factor;
+      }
+    }
+    for(let y = 0; y < height; y++){
+      for(let x = 0; x < width; x++){
+        let index = (y * width + x) * 4;
+        
+        let oldR = data[index];
+        let oldG = data[index + 1];
+        let oldB = data[index + 2];
+        
+        const [newR, newG, newB] = findClosestColor(oldR, oldG, oldB, pallete);
+
+        data[index] = newR;
+        data[index + 1] = newG;
+        data[index + 2] = newB;
+
+        let errR = oldR - newR;
+        let errG = oldG - newG;
+        let errB = oldB - newB;
+        
+        calculateError(x+1, y, errR, errG, errB, 7/16);
+        calculateError(x-1, y+1, errR, errG, errB, 3/16);
+        calculateError(x, y+1, errR, errG, errB, 5/16);
+        calculateError(x+1, y+1, errR, errG, errB, 1/16);
+      }
+    }
+  }
+
+  
+  const drawCanvas = (img, algorithm) => {
+    const canvas = canvasRef.current;
+    if(!canvas) return;
+
+    const ctx = canvas.getContext("2d");
+    let width = img.width;
+    let height = img.height;
+    canvas.width = width;
+    canvas.height = height;
+
+    ctx.drawImage(img, 0, 0);
+
+    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+    console.log(data); //look
+
+    if(algorithm == "Floyd-Steinberg"){
+      floydsteinberg(data, width, height, pallete);
+    }
+    ctx.putImageData(imageData, 0, 0);
+  }
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    if(!file) return;
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const img = new Image();
+      img.onload = () => {
+        loadedImgRef.current = img;
+        drawCanvas(img, algorithmSelected);
+        setImageSrc(e.target.result);
+      }
+      img.src = e.target.result;
+    }
+    reader.readAsDataURL(file)
+  }
+  const handleAlgorithmChange = (event) => {
+    const value = event.target.value;
+    setAlgorithmSelected(value);
+    if(loadedImgRef.current) {
+      drawCanvas(loadedImgRef.current, value);
+    }
+  }
+  const handleExport = () => {
+    const canvas = canvasRef.current;
+    if(!canvas) return;
+
+    const link = document.createElement("a");
+    link.download = "export.png";
+    link.href = canvas.toDataURL("image/png");
+    link.click();
+  }
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.js
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div>
+      <h1>Posterize</h1>
+
+      <div>
+        <label>
+          Select image:{" "}
+          <input type="file" accept="image/*" onChange={handleImageUpload} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Algorithm:{" "}
+          <select value={algorithmSelected} onChange={handleAlgorithmChange}>
+            {algorithms.map((option, index) => (
+              <option value={option} key={index}>{option}</option>
+            ))}
+          </select>
+        </label>
+      </div>
+      <div>
+        <canvas ref={canvasRef}></canvas>
+      </div>
+
+      {imageSrc && <button onClick={handleExport}>export</button>}
     </div>
-  );
+  )
 }
